8282fcd08e3e6e56f5a0c97c8511049630040745
Tjblues1 <tjblues4real@yahoo.com>
2022-12-03 08:19:50 -0800

    task5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
+from variable_load_5 import a
+
+
+def main():
     print(a)
+
+if __name__ == "__main__":
+    main()
