a6d177a20a2a66c1e6eda61ed54bcc866ebcd67f
Tjblues1 <tjblues4real@yahoo.com>
2022-12-03 07:44:59 -0800

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-
-    lenght = len(sys.argv) - 1
-    if lenght == 0:
+    from sys import argv
+    if len(argv) == 1:
         print("0 arguments.")
-    elif lenght == 1:
-        print("1 argument:")
+    elif len(argv) == 2:
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(lenght))
-    for index in range(lenght):
-        print("{}: {}".format(index + 1, sys.argv[index + 1]))
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-lenght = len(sys.argv) - 1
-sum = 0
-if lenght == 0:
-    print("0")
-else:
-    for i in range(lenght):
-        sum += int(sys.argv[i + 1])
-   print("{}".format(sum))
+    sum = 0
+    from sys import argv
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print(sum)
